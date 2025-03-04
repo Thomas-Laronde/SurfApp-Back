a47485f16f0a4fcd5c1b6f49e6d725ce2859d48f
Thomas Chevaillier <laronde.chevaillier@gmail.com>
2025-03-04 13:25:40 +0100

    Adding Maven project

---
+++ src/main/java/com/surfapp/SurfApp_Back/SurfAppBackApplication.java
@@ -0,0 +1,13 @@
+package com.surfapp.SurfApp_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurfAppBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurfAppBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surfapp/SurfApp_Back/SurfAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surfapp.SurfApp_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurfAppBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
